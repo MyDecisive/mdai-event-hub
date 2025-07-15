ef6dae2f72f73883fb038c90489c908344975b34
Robin Law <Robin-Law@users.noreply.github.com>
2025-07-15 16:37:54 -0500

    much more handlers testing

--- types.go
+++ types.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-	datacore "github.com/decisiveai/mdai-data-core/handlers"
+	"context"
 	"github.com/decisiveai/mdai-event-hub/eventing"
 	"go.uber.org/zap"
 )
 
+type IHandlerAdapter interface {
+	AddElementToSet(ctx context.Context, variableKey string, hubName string, value string, correlationId string) error
+	RemoveElementFromSet(ctx context.Context, variableKey string, hubName string, value string, correlationId string) error
+	AddSetMapElement(ctx context.Context, variableKey string, hubName string, field string, value string, correlationId string) error
+	RemoveElementFromMap(ctx context.Context, variableKey string, hubName string, field string, correlationId string) error
+	SetStringValue(ctx context.Context, variableKey string, hubName string, value string, correlationId string) error
+}
+
 type MdaiInterface struct {
 	logger *zap.Logger
-	data   *datacore.HandlerAdapter
+	data   IHandlerAdapter
 }
 
 type HandlerName string
